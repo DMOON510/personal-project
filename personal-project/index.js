a119b05f4cb26fed3ad7a54e8972f75a300418bc
RakenAnn <41386360+RakenAnn@users.noreply.github.com>
2019-07-17 15:19:49 -0700

    functional buttons

--- personal-project/index.js
+++ personal-project/index.js
@@ -1,27 +1,37 @@
-// var buttonOne = $(".firstButtons");
-//
-// buttonOne.on('click', hideButtons);
-//
-// function hideButtons() {
-//     // button.toggleClass('firstButtons');
-//     buttonOne.css("display", "none");
-// }
-//
-// var sweet = $(".sweetButtons")
-// function hideSbuttons() {
-//   buttonTwo.css("display","none");
-// }
-//  console.log("hello")
-
+// homescreen buttons
 var buttonOne = $(".firstButtons");
 buttonOne.on('click', hideButtons);
 
 function hideButtons() {
-    buttonOne.css("display", "none");
+  buttonOne.css("display", "none");
 }
 
+// to show the sweet buttons
 var buttonTwo = $(".sweetButtons");
+var initSweet = $("#sweetInit");
 
-function showButtons() {
-  buttonTwo.toggleClass('.hide');
+initSweet.on('click', showButtonsSweet);
+
+function showButtonsSweet() {
+  buttonTwo.css("display", "initial");
+}
+
+// to show the savory buttons
+var buttonThree = $(".savoryButtons");
+var initSavory = $("#savoryInit");
+
+initSavory.on('click', showButtonsSavory);
+
+function showButtonsSavory() {
+  buttonThree.css("display", "initial");
+}
+
+// to show the beverage buttons
+var buttonFour = $(".beverageButtons");
+var initBeverage = $("#beverageInit");
+
+initBeverage.on('click', showButtonsBeverages);
+
+function showButtonsBeverages() {
+  buttonFour.css("display", "initial");
 }
